cd1d462a04b57ec2907c309be82238c81fafdee8
Jerome Bakker <jeabakker@coldtrick.com>
2014-07-17 09:55:05 +0200

    dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file was created by Translation Editor v2.0
+ * On 2014-07-17 09:54
+ */
+
+$language = array (
+  'profile_sync' => 'Profile Sync',
+  'admin:configure_utilities:profile_sync' => 'Profile Sync',
+  'item:object:profile_sync_datasource' => 'Profile Sync databron',
+  'item:object:profile_sync_config' => 'Prorile Sync configuratie',
+  'profile_sync:sync_config:run' => 'Sync nu',
+  'profile_sync:sync_configs:schedule:manual' => 'Handmatig',
+  'profile_sync:admin:datasources' => 'Databronnen',
+  'profile_sync:admin:datasources:add' => 'Nieuwe databron',
+  'profile_sync:admin:sync_configs' => 'Sync configuraties',
+  'profile_sync:admin:sync_configs:add' => 'Nieuwe sync configuratie',
+  'profile_sync:admin:datasources:edit:mysql:dbhost' => 'Database host',
+  'profile_sync:admin:datasources:edit:mysql:dbport' => 'Database poort',
+  'profile_sync:admin:datasources:edit:mysql:dbport:default' => 'Standaard: 3306',
+  'profile_sync:admin:datasources:edit:mysql:dbname' => 'Database naam',
+  'profile_sync:admin:datasources:edit:mysql:dbusername' => 'Database gebruikersnaam',
+  'profile_sync:admin:datasources:edit:mysql:dbpassword' => 'Database wachtwoord',
+  'profile_sync:admin:datasources:edit:mysql:dbquery' => 'Database query',
+  'profile_sync:admin:sync_configs:edit:no_columns' => 'Geen kolommen gevonden in de databron.',
+  'profile_sync:admin:sync_configs:edit:datasource' => 'Gebruikt databron',
+  'profile_sync:admin:sync_configs:edit:select_datasource_column' => 'Selecteer een databron kolom',
+  'profile_sync:admin:sync_configs:edit:select_profile_column' => 'Selecteer een doel profielveld',
+  'profile_sync:admin:sync_configs:edit:fields' => 'Veld configuratie',
+  'profile_sync:admin:sync_configs:edit:unique_id' => 'Uniek identificatie veld',
+  'profile_sync:admin:sync_configs:edit:schedule' => 'Schema',
+  'profile_sync:admin:sync_configs:edit:create_user' => 'Maak ontbrekende gebruikers aan',
+  'profile_sync:admin:sync_configs:edit:create_user:description' => 'Als een gebruiker niet kon worden gevonden, maak deze aan. Dit vereist een veld voor Weergave naam, gebruikersnaam en e-mail.',
+  'profile_sync:admin:sync_configs:edit:notify_user' => 'Stuur de nieuwe gebruiker een bericht met de gebruikersnaam en wachtwoord',
+  'profile_sync:sync_config:logs' => 'Bekijk logs',
+  'profile_sync:sync_logs:title' => 'Sync log: %s',
+  'profile_sync:view_log:title' => 'Log bestand voor %s van %s',
+  'profile_sync:action:error:title' => 'Geef een titel op',
+  'profile_sync:action:datasource:edit:error:params' => 'Geen parameters opgegeven, controleer het formulier',
+  'profile_sync:action:datasource:edit:error:entity' => 'Er kon geen databron worden gevonden/aangemaakt, probeer het nogmaals',
+  'profile_sync:action:sync_config:edit:error:guid' => 'Ongeldige GUID opgegeven, probeer het nogmaals',
+  'profile_sync:action:sync_config:edit:error:unique_id' => 'Geef een set van unieke identificatie velden op',
+  'profile_sync:action:sync_config:edit:error:fields' => 'Er zijn geen velden geconfigureerd voor synchronisatie',
+  'profile_sync:action:sync_config:edit:error:entity' => 'De opgegeven GUID is geen synchronisatie configuratie',
+  'profile_sync:action:sync_config:run' => 'Synchronisatie run',
+);
+add_translation("nl", $language);

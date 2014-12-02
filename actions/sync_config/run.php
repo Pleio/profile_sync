36bb5970d37aafadcd13f48aea12bb1e471cf6b5
Jerome Bakker <jeabakker@coldtrick.com>
2014-12-02 10:49:42 +0100

    set correct memory limits on manual run

--- actions/sync_config/run.php
+++ actions/sync_config/run.php
@@ -14,7 +14,20 @@ if (empty($entity) || !elgg_instanceof($entity, "object", "profile_sync_config")
 	forward(REFERER);
 }
 
+// get current memory limit
+$old_memory_limit = ini_get("memory_limit");
+
+// set new memory limit
+$setting = elgg_get_plugin_setting("memory_limit", "profile_sync");
+if (!empty($setting)) {
+	ini_set("memory_limit", $setting);
+}
+
 profile_sync_proccess_configuration($entity);
+
+// reset memory limit
+ini_set("memory_limit", $old_memory_limit);
+
 system_message(elgg_echo("profile_sync:action:sync_config:run"));
 
 forward(REFERER);

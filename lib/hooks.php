627c8bae8cf9000367d3d6aee02da6e00e457d4f
Jerome Bakker <jeabakker@coldtrick.com>
2015-07-01 14:41:45 +0200

    changed: quick return in hook

--- lib/hooks.php
+++ lib/hooks.php
@@ -11,93 +11,98 @@
  * @param ElggMenuItem[] $return the current menu items
  * @param array          $params parameters
  *
- * @return ElggMenuItem[]
+ * @return void|ElggMenuItem[]
  */
 function profile_sync_entity_register_menu($hook, $type, $return, $params) {
 	
-	if (!empty($params) && is_array($params)) {
-		$entity = elgg_extract("entity", $params);
+	if (empty($params) || !is_array($params)) {
+		return;
+	}
+	
+	$entity = elgg_extract("entity", $params);
+	if (empty($entity) || !elgg_instanceof($entity, 'object')) {
+		return;
+	}
+	
+	if (elgg_instanceof($entity, "object", "profile_sync_datasource")) {
+		elgg_load_js("lightbox");
+		elgg_load_css("lightbox");
 		
-		if (elgg_instanceof($entity, "object", "profile_sync_datasource")) {
-			elgg_load_js("lightbox");
-			elgg_load_css("lightbox");
-			
-			foreach ($return as $key => $menu_item) {
-				$name = $menu_item->getName();
-				switch ($name) {
-					case "edit":
-						
-						$menu_item->setHref("ajax/view/profile_sync/forms/datasource?guid=" . $entity->getGUID());
-						$menu_item->setLinkClass("elgg-lightbox");
-						$menu_item->setTooltip("");
-						break;
-					case "delete":
-						break;
-					default:
-						unset($return[$key]);
-						break;
-				}
+		foreach ($return as $key => $menu_item) {
+			$name = $menu_item->getName();
+			switch ($name) {
+				case "edit":
+					
+					$menu_item->setHref("ajax/view/profile_sync/forms/datasource?guid=" . $entity->getGUID());
+					$menu_item->setLinkClass("elgg-lightbox");
+					$menu_item->setTooltip("");
+					break;
+				case "delete":
+					break;
+				default:
+					unset($return[$key]);
+					break;
 			}
-			
-			$return[] = ElggMenuItem::factory(array(
-				"name" => "add_sync_config",
-				"text" => elgg_echo("profile_sync:admin:sync_configs:add"),
-				"href" => "ajax/view/profile_sync/forms/sync_config?datasource_guid=" . $entity->getGUID(),
-				"priority" => 10,
-				"link_class" => "elgg-lightbox"
-			));
-		} elseif (elgg_instanceof($entity, "object", "profile_sync_config")) {
-			elgg_load_js("lightbox");
-			elgg_load_css("lightbox");
-			
-			foreach ($return as $key => $menu_item) {
-				$name = $menu_item->getName();
-				switch ($name) {
-					case "edit":
-			
-						$menu_item->setHref("ajax/view/profile_sync/forms/sync_config?guid=" . $entity->getGUID());
-						$menu_item->setLinkClass("elgg-lightbox");
-						$menu_item->setTooltip("");
-						break;
-					case "delete":
-						break;
-					default:
-						unset($return[$key]);
-						break;
-				}
-			}
-			
-			$schedule_text = elgg_echo("profile_sync:interval:" . $entity->schedule);
-			if ($entity->schedule == "manual") {
-				$schedule_text = elgg_echo("profile_sync:sync_configs:schedule:manual");
-			}
-			$return[] = ElggMenuItem::factory(array(
-				"name" => "sync_config_interval",
-				"text" => $schedule_text,
-				"href" => false,
-				"priority" => 10,
-			));
-			
-			$return[] = ElggMenuItem::factory(array(
-				"name" => "run",
-				"text" => elgg_echo("profile_sync:sync_config:run"),
-				"href" => "ajax/view/profile_sync/sync_config/run?guid=" . $entity->getGUID(),
-				"priority" => 50,
-				"is_action" => true,
-				"link_class" => "elgg-lightbox"
-			));
-			
-			if ($entity->lastrun) {
-				$return[] = ElggMenuItem::factory(array(
-					"name" => "logs",
-					"text" => elgg_echo("profile_sync:sync_config:logs"),
-					"href" => "ajax/view/profile_sync/sync_logs/?guid=" . $entity->getGUID(),
-					"priority" => 100,
-					"link_class" => "elgg-lightbox"
-				));
-			}
-			
 		}
+		
+		$return[] = ElggMenuItem::factory(array(
+			"name" => "add_sync_config",
+			"text" => elgg_echo("profile_sync:admin:sync_configs:add"),
+			"href" => "ajax/view/profile_sync/forms/sync_config?datasource_guid=" . $entity->getGUID(),
+			"priority" => 10,
+			"link_class" => "elgg-lightbox"
+		));
+	} elseif (elgg_instanceof($entity, "object", "profile_sync_config")) {
+		elgg_load_js("lightbox");
+		elgg_load_css("lightbox");
+		
+		foreach ($return as $key => $menu_item) {
+			$name = $menu_item->getName();
+			switch ($name) {
+				case "edit":
+		
+					$menu_item->setHref("ajax/view/profile_sync/forms/sync_config?guid=" . $entity->getGUID());
+					$menu_item->setLinkClass("elgg-lightbox");
+					$menu_item->setTooltip("");
+					break;
+				case "delete":
+					break;
+				default:
+					unset($return[$key]);
+					break;
+			}
+		}
+		
+		$schedule_text = elgg_echo("profile_sync:interval:" . $entity->schedule);
+		if ($entity->schedule == "manual") {
+			$schedule_text = elgg_echo("profile_sync:sync_configs:schedule:manual");
+		}
+		$return[] = ElggMenuItem::factory(array(
+			"name" => "sync_config_interval",
+			"text" => $schedule_text,
+			"href" => false,
+			"priority" => 10,
+		));
+		
+		$return[] = ElggMenuItem::factory(array(
+			"name" => "run",
+			"text" => elgg_echo("profile_sync:sync_config:run"),
+			"href" => "ajax/view/profile_sync/sync_config/run?guid=" . $entity->getGUID(),
+			"priority" => 50,
+			"is_action" => true,
+			"link_class" => "elgg-lightbox"
+		));
+		
+		if ($entity->lastrun) {
+			$return[] = ElggMenuItem::factory(array(
+				"name" => "logs",
+				"text" => elgg_echo("profile_sync:sync_config:logs"),
+				"href" => "ajax/view/profile_sync/sync_logs/?guid=" . $entity->getGUID(),
+				"priority" => 100,
+				"link_class" => "elgg-lightbox"
+			));
+		}
+		
 	}
 	
 	return $return;

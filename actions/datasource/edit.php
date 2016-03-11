48ec1005609266717b573b7b6590985e23bc5ee8
Jerome Bakker <jeabakker@coldtrick.com>
2016-03-11 10:04:30 +0100

    chore: coding style

--- actions/datasource/edit.php
+++ actions/datasource/edit.php
@@ -1,31 +1,31 @@
 <?php
 
-$guid = (int) get_input("guid");
-$params = get_input("params", array(), false);
-$title = get_input("title");
+$guid = (int) get_input('guid');
+$params = get_input('params', [], false);
+$title = get_input('title');
 
 if (!is_array($params)) {
-	register_error(elgg_echo("profile_sync:action:datasource:edit:error:params"));
+	register_error(elgg_echo('profile_sync:action:datasource:edit:error:params'));
 	forward(REFERER);
 }
 
 if (empty($title)) {
-	register_error(elgg_echo("profile_sync:action:error:title"));
+	register_error(elgg_echo('profile_sync:action:error:title'));
 	forward(REFERER);
 }
 
-$datasource_type = elgg_extract("datasource_type", $params);
+$datasource_type = elgg_extract('datasource_type', $params);
 if (empty($datasource_type)) {
-	register_error(elgg_echo("profile_sync:action:datasource:edit:error:type"));
+	register_error(elgg_echo('profile_sync:action:datasource:edit:error:type'));
 	forward(REFERER);
 }
 
 $entity = get_entity($guid);
-if (!elgg_instanceof($entity, "object", "profile_sync_datasource")) {
+if (!elgg_instanceof($entity, 'object', 'profile_sync_datasource')) {
 	$site = elgg_get_site_entity();
 	
 	$entity = new ElggObject();
-	$entity->subtype = "profile_sync_datasource";
+	$entity->subtype = 'profile_sync_datasource';
 	$entity->owner_guid = $site->getGUID();
 	$entity->container_guid = $site->getGUID();
 	$entity->access_id = ACCESS_PUBLIC;
@@ -40,9 +40,9 @@ if ($entity) {
 	$entity->title = $title;
 	
 	// some inputs need to be unfiltered
-	$unfiltered_params = array(
+	$unfiltered_params = [
 		'dbquery',
-	);
+	];
 	
 	foreach ($params as $key => $param) {
 		// filter input
@@ -58,9 +58,9 @@ if ($entity) {
 	}
 	
 	$entity->save();
-	system_message(elgg_echo("admin:configuration:success"));
+	system_message(elgg_echo('admin:configuration:success'));
 } else {
-	register_error(elgg_echo("profile_sync:action:datasource:edit:error:entity"));
+	register_error(elgg_echo('profile_sync:action:datasource:edit:error:entity'));
 }
 	
 forward(REFERER);
